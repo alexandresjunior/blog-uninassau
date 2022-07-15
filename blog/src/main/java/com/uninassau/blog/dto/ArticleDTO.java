f4d70504cac2cd46b36684045bd79184e3085ec0
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-15 06:34:31 -0300

    Define DTO object class

---
+++ blog/src/main/java/com/uninassau/blog/dto/ArticleDTO.java
@@ -0,0 +1,45 @@
+package com.uninassau.blog.dto;
+
+/**
+ * @author Alexandre de Souza Jr.
+ */
+public class ArticleDTO {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    private String title;
+    private String author;
+    private String date;
+    private String text;
+
+}
