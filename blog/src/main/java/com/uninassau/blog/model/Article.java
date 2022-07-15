791e40d293bde266b93a89798ebff895729335b4
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-07-15 06:32:29 -0300

    Define article model/entity

---
+++ blog/src/main/java/com/uninassau/blog/model/Article.java
@@ -0,0 +1,68 @@
+package com.uninassau.blog.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Alexandre de Souza Jr.
+ */
+@Entity
+public class Article {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String title;
+    private String author;
+    private Date date;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+}
